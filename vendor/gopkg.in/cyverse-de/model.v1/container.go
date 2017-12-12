e7cec9b35743bd649e4291bc2cdd227a5ee54650
Paul Sarando <psarando@cyverse.org>
2017-12-12 14:00:05 -0700

    CORE-8886 Update model to v1.1

--- vendor/gopkg.in/cyverse-de/model.v1/container.go
+++ vendor/gopkg.in/cyverse-de/model.v1/container.go
@@ -49,6 +49,7 @@ type Container struct {
 	MinMemoryLimit int64          `json:"min_memory_limit"` // The minimum the container needs.
 	MinCPUCores    int            `json:"min_cpu_cores"`    // The minimum number of cores the container needs.
 	MinDiskSpace   int64          `json:"min_disk_space"`   // The minimum amount of disk space that the container needs.
+	PIDsLimit      int64          `json:"pids_limit"`
 	Image          ContainerImage `json:"image"`
 	EntryPoint     string         `json:"entrypoint"`
 	WorkingDir     string         `json:"working_directory"`
